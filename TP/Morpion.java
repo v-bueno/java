36ee313b914179eadd19c48738b9fb0664fe0fe6
BUENO Vincent <vbueno@enssat.fr>
2023-05-03 15:00:39 +0200

    Morpion TP

---
+++ TP/Morpion.java
@@ -0,0 +1,25 @@
+public class Morpion {
+    public static void main(String[] args) {
+        int[][] matrice = new int[3][];
+        int[] ligne1 = new int[3];
+        ligne1[0] = 0;
+        ligne1[1] = 0;
+        ligne1[2] = 0;
+        int[] ligne2 = new int[3];
+        ligne2[0] = 0;
+        ligne2[1] = 0;
+        ligne2[2] = 0;
+        int[] ligne3 = new int[3];
+        ligne3[0] = 0;
+        ligne3[1] = 0;
+        ligne3[2] = 0;
+
+        matrice[0] = ligne1;
+        matrice[1] = ligne2;
+        matrice[2] = ligne3;
+    }
+
+    public static void affiche(int[][] matrice){
+
+    }
+}

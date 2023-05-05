b659a9d86552246ca89db320e027f5fc2c2c4b6c
BUENO Vincent <vbueno@enssat.fr>
2023-05-05 11:05:41 +0200

    POINT2D Question 1

--- TP/Point2D.java
+++ TP/Point2D.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Point2D {
+public class Point2D {
+
+    private int x;
+
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Point2D point2D)) return false;
+        return getX() == point2D.getX() && getY() == point2D.getY();
+    }
+
+    @Override
+    public String toString() {
+        return "Point2D{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
